437c25acfc021636c16b5be8c65a7e31b4057499
Vincent LAURENT <vince.laurent@gmail.com>
2018-11-28 17:30:46 +0100

    Timeout de 10 sec

--- web/js/main.js
+++ web/js/main.js
@@ -7,7 +7,7 @@
 
                 var options = {
                   enableHighAccuracy: true,
-                  timeout: 5000,
+                  timeout: 10000,
                   maximumAge: 0
                 };
 
